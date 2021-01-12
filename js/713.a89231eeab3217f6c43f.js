583af9eb2fbae5e97d9561ebf6dfa98f70896fdb
Travis CI User <travis@example.org>
2021-01-12 08:03:45 +0000

    Landing nav (#174)
    * Remove PF babel cjs mapping.
    
    * Add chubk names to lazy loaded routes.
    
    * Update landing layout to accomodate the left nav.
    
    * Create navigation focus trigger mechanism.
    
    * Add navigation overlay component.
    
    * Add nav overlay sticky syling to nav item.
    
    * Udpdate PF
    
    * Fixed missing overlay divider.
    
    * Fix tests after layout changes.
    
    * Remove landing nav from unauthed page.
    
    * Temporarily exclude qa-beta from custom release.

---
+++ js/713.a89231eeab3217f6c43f.js
@@ -0,0 +1,2 @@
+(self.webpackChunklanding_page_frontend=self.webpackChunklanding_page_frontend||[]).push([[713],{59713:e=>{e.exports=function(e,n,a){return n in e?Object.defineProperty(e,n,{value:a,enumerable:!0,configurable:!0,writable:!0}):e[n]=a,e}}}]);
+//# sourceMappingURL=713.a89231eeab3217f6c43f.js.map
