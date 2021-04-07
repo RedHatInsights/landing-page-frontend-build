3113703893fc6447567cbfda1e0f10db5978a969
Travis CI User <travis@example.org>
2021-04-07 15:08:56 +0000

    Use grid layout for footer tiles. (#230)

--- js/277.1a128997c5131cee0c77.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunklanding_page_frontend=self.webpackChunklanding_page_frontend||[]).push([[277],{76277:(e,n,a)=>{"use strict";a.r(n),a.d(n,{default:()=>d.default});var d=a(62354)}}]);
---
+++ js/675.aafc921320f76f66f9dd.js
@@ -0,0 +1,1 @@
+(self.webpackChunklanding_page_frontend=self.webpackChunklanding_page_frontend||[]).push([[675],{18675:(e,n,a)=>{"use strict";a.r(n),a.d(n,{default:()=>d.default});var d=a(3376)}}]);
