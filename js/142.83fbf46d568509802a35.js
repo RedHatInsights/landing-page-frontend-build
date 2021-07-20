aa969c0efbd126af7589b7e579c93c77739149d1
Travis CI User <travis@example.org>
2021-07-20 23:54:53 +0000

    update patternfly libraries (#90)
    * support for building on node12
    
    * small README mod
    
    * udpate PF libraries
    
    * tests, yah

--- js/142.83fbf46d568509802a35.js
+++ js/142.83fbf46d568509802a35.js
@@ -1,2 +1,2 @@
 (self.webpackChunklanding_page_frontend=self.webpackChunklanding_page_frontend||[]).push([[142],{3142:(e,n,t)=>{"use strict";t.r(n),t.d(n,{default:()=>f});var r=t(2950),a=t.n(r),l=t(7194),o=t(9971),c=t(4685),s=t(319),u=t(1824),g=t(5697),p=t.n(g),i=window.location.pathname.split("/");i.shift();var d=function(e){var n=e.logger;return a().createElement(o.Provider,{store:(0,s.S)(n).getStore()},a().createElement(a().Fragment,null,a().createElement(l.BrowserRouter,{basename:"/".concat("beta"===i[0]?"beta/":"")},a().createElement(u.Z,null)),a().createElement(c.ZP,null)))};d.propTypes={logger:p().func};const f=d}}]);
-//# sourceMappingURL=../sourcemaps/142.fc82d53f99c8a7ab57c8.js.map
+//# sourceMappingURL=../sourcemaps/142.d0fceba3bf8e1ee563ba.js.map
