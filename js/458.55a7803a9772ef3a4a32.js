fc5aa254546f3f21e2cbd9bbd68005dc17bfa485
Travis CI User <travis@example.org>
2021-08-02 09:44:44 +0000

    Merge pull request #326 from Hyperkid123/enable-chrome-2
    Enable chrome 2 rendering.

--- js/458.55a7803a9772ef3a4a32.js
+++ js/458.55a7803a9772ef3a4a32.js
@@ -1,2 +1,2 @@
-(self.webpackChunklanding_page_frontend=self.webpackChunklanding_page_frontend||[]).push([[458],{2246:(e,n,t)=>{"use strict";t.r(n);var r=t(2950),a=t.n(r),l=t(2181),d=t.n(l),u=t(9535);d().render(a().createElement(u.Z,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/458.bf07a90b5dddc3886ebd.js.map
+"use strict";(self.webpackChunklanding_page_frontend=self.webpackChunklanding_page_frontend||[]).push([[458],{2246:(e,n,t)=>{t.r(n);var r=t(2950),a=t.n(r),l=t(2181),d=t.n(l),u=t(9535);d().render(a().createElement(u.Z,null),document.getElementById("root"))}}]);
+//# sourceMappingURL=../sourcemaps/458.0d253dc3221077eb7b3a.js.map
