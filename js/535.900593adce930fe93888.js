a73bb4fd2520ca3e2e67b7bba80866e56b706075
Travis CI User <travis@example.org>
2021-08-07 00:03:55 +0000

    update patternfly libraries (#90)
    * support for building on node12
    
    * small README mod
    
    * udpate PF libraries
    
    * tests, yah

--- js/535.900593adce930fe93888.js
+++ js/535.900593adce930fe93888.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunklanding_page_frontend=self.webpackChunklanding_page_frontend||[]).push([[535],{9535:(e,n,t)=>{t.d(n,{Z:()=>c});var a=t(3212),o=t(2950),i=t.n(o);const c=function(){return i().createElement("section",{className:"pf-l-page__main-section pf-c-page__main-section ins-c-page__logout"},i().createElement(a.Title,{headingLevel:"h1",size:"3xl"},"You have logged out."),i().createElement(a.Button,{variant:"link",onClick:function(){return"",void(window.location.href="./".concat(""))}},"Take me home"))}}}]);
-//# sourceMappingURL=../sourcemaps/535.804da61add53423ee9c2.js.map
+//# sourceMappingURL=../sourcemaps/535.1eb2d3177d6d91ec3652.js.map
