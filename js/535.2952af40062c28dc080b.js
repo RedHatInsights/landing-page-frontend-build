0d1d905ae71ad3724beaf37118db63c52d42160a
Travis CI User <travis@example.org>
2021-08-09 12:53:52 +0000

    Clean up recommendation accordion (#334)
    * Open rhel accordion by default.
    
    * Add spacing to recommendation entry loader.
    
    * Update ansible content.
    
    * Remove unecessary scrollbars
    
    * Align accordion button with scrollbar.

--- js/535.2952af40062c28dc080b.js
+++ js/535.2952af40062c28dc080b.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunklanding_page_frontend=self.webpackChunklanding_page_frontend||[]).push([[535],{9535:(e,n,t)=>{t.d(n,{Z:()=>c});var a=t(2413),o=t(2950),i=t.n(o);const c=function(){return i().createElement("section",{className:"pf-l-page__main-section pf-c-page__main-section ins-c-page__logout"},i().createElement(a.Title,{headingLevel:"h1",size:"3xl"},"You have logged out."),i().createElement(a.Button,{variant:"link",onClick:function(){return"",void(window.location.href="./".concat(""))}},"Take me home"))}}}]);
-//# sourceMappingURL=../sourcemaps/535.d68e0a0e81ae5531a31c.js.map
+//# sourceMappingURL=../sourcemaps/535.a928230e72556b6eda63.js.map
