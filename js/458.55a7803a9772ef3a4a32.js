11c84cbc98c51c56d0d6a8e0a6cb050b15fd06b9
Travis CI User <travis@example.org>
2021-08-11 20:17:49 +0000

    Swap Recommendations and Insights Cards (#338)

--- js/458.55a7803a9772ef3a4a32.js
+++ js/458.55a7803a9772ef3a4a32.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunklanding_page_frontend=self.webpackChunklanding_page_frontend||[]).push([[458],{2246:(e,n,t)=>{t.r(n);var r=t(2950),a=t.n(r),l=t(2181),d=t.n(l),u=t(9535);d().render(a().createElement(u.Z,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/458.fb93997fce7ddb47f1e0.js.map
+//# sourceMappingURL=../sourcemaps/458.2c16315c22bcaa4bf0ef.js.map
