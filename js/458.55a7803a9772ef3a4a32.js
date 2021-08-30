75b326d5ddf31144f007cd4c8b7566e1fc9864a0
Travis CI User <travis@example.org>
2021-08-30 14:15:30 +0000

    margin fix (#345)

--- js/458.55a7803a9772ef3a4a32.js
+++ js/458.55a7803a9772ef3a4a32.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunklanding_page_frontend=self.webpackChunklanding_page_frontend||[]).push([[458],{2246:(e,n,t)=>{t.r(n);var r=t(2950),a=t.n(r),l=t(2181),d=t.n(l),u=t(9535);d().render(a().createElement(u.Z,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/458.67274e4008a680ef9030.js.map
+//# sourceMappingURL=../sourcemaps/458.e5d9aaeebe0c536e0979.js.map
