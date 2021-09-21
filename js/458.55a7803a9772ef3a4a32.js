6274f48461f10bbe14f129d910a272cb2f445391
Travis CI User <travis@example.org>
2021-09-21 12:51:38 +0000

    Merge pull request #354 from epwinchell/land_prefix
    Replace "ins-" prefix with "land"

--- js/458.55a7803a9772ef3a4a32.js
+++ js/458.55a7803a9772ef3a4a32.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunklanding_page_frontend=self.webpackChunklanding_page_frontend||[]).push([[458],{2246:(e,n,t)=>{t.r(n);var r=t(2950),a=t.n(r),l=t(2181),d=t.n(l),u=t(9535);d().render(a().createElement(u.Z,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/458.ee71be7506c02d3c1979.js.map
+//# sourceMappingURL=../sourcemaps/458.2fececa03d775351e9ac.js.map
