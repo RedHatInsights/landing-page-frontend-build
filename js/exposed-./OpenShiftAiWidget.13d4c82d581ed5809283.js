914c78ee8c4ad19ad857a70c2f93a233e6c53375
Travis CI User <travis@example.org>
2024-05-14 06:46:33 +0000

    Merge pull request #575 from karelhala/adjust-app-services-links
    Adjust app services links to point to the right place

---
+++ js/exposed-./OpenShiftAiWidget.13d4c82d581ed5809283.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklanding=self.webpackChunklanding||[]).push([[6491],{82369:(e,n,t)=>{t.r(n),t.d(n,{default:()=>r});var i=t(28416),a=t.n(i),l=t(95066);const r=()=>a().createElement(a().Fragment,null,a().createElement(l.Z,{id:6,body:"Create, train, and serve artificial intelligence and machine learning (AI/ML) models.",linkTitle:"OpenShift AI",url:"https://www.redhat.com/en/technologies/cloud-computing/openshift/openshift-ai/trial",isExternal:!0}))}}]);
+//# sourceMappingURL=../../sourcemaps/exposed-./OpenShiftAiWidget.46599df282a3594eba7f81c8824a9ca9.js.map
